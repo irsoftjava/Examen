41557fc9a823531059452deb0ae8f01fd5565a21
Ivan <javaspringcga@gmail.com>
2023-09-12 19:22:33 +0100

    Se actualizó el ejercicio5

--- src/main/java/ejercicio5/EstudianteInt.java
+++ src/main/java/ejercicio5/EstudianteInt.java
@@ -1,4 +1,34 @@
 package ejercicio5;
 
-public class EstudianteInt {
+/**
+ * Clase EstudianteInt
+ */
+public class EstudianteInt extends Estudiante {
+
+    /**
+     * Atributos
+     */
+    private Boolean esErasmus;
+    private String nacionalidad;
+
+    /**
+     * Constructor vacío
+     */
+    public EstudianteInt() {
+    }
+
+    /**
+     * Constructor Parametrizado
+     * @param nombre
+     * @param direccion
+     * @param edad
+     * @param codigo
+     * @param esErasmus
+     * @param nacionalidad
+     */
+    public EstudianteInt(String nombre, String direccion, int edad, String codigo, Boolean esErasmus, String nacionalidad) {
+        super(nombre, direccion, edad, codigo);
+        this.esErasmus = esErasmus;
+        this.nacionalidad = nacionalidad;
+    }
 }
