e2dbd3cc8aef10ac8d9f07046e8884f768941469
Ivan <javaspringcga@gmail.com>
2023-09-12 19:26:11 +0100

    Se actualizó el ejercicio5

--- src/main/java/ejercicio5/EstudianteInt.java
+++ src/main/java/ejercicio5/EstudianteInt.java
@@ -31,4 +31,43 @@ public class EstudianteInt extends Estudiante {
         this.esErasmus = esErasmus;
         this.nacionalidad = nacionalidad;
     }
+
+    /**
+     * Métodos
+     * @return
+     */
+    public Boolean getEsErasmus() {
+        return esErasmus;
+    }
+
+    public void setEsErasmus(Boolean esErasmus) {
+        this.esErasmus = esErasmus;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public boolean esEuropeo() {
+        return true;
+    }
+
+    public boolean descuentosParaJovenes() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EstudianteInt{" +
+                "esErasmus=" + esErasmus +
+                ", nacionalidad='" + nacionalidad + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
 }
