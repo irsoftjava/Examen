7b12add1cbe63f996b65efa5344d76cc576ef017
Ivan <javaspringcga@gmail.com>
2023-09-13 15:54:55 +0100

    Se actualizó el ejercicio5

--- src/main/java/ejercicio5/AppEstudiantes.java
+++ src/main/java/ejercicio5/AppEstudiantes.java
@@ -10,6 +10,8 @@ public class AppEstudiantes {
         Estudiante e1 = new Estudiante("Ernesto Garcia", "C/ Alvarado #34", 24, "5A3C");
         Estudiante e2 = new Estudiante("Maria Julia", "C/ San Fernando #88", 26, "5A8F");
 
+        System.out.println();
+        System.out.println("Estudiantes");
         System.out.println(e1);
         System.out.println(e2);
 
@@ -21,6 +23,7 @@ public class AppEstudiantes {
         estudiantesInternacionales.add(new EstudianteInt("Amador Lopez", "C/ Avellaneda #11", 21, "1I15", true, Nacionalidad.FRANCES.toString() ));
         estudiantesInternacionales.add(new EstudianteInt("Juan Antonio", "C/ Fernando de Sayaz #353", 20, "1I16", false, Nacionalidad.AMERICANO.toString() ));
 
+        System.out.println("Estudiantes Internacionales con descuentos para Jóvenes.");
         for (EstudianteInt ei : estudiantesInternacionales) {
             if (ei.esEuropeo() && ei.descuentosParaJovenes()) {
                 System.out.println(ei);
